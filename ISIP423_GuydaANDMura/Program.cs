c6921f43b42a198df6767424546e02c58a2f1aac
codemachine52 <82andrik887@mail.ru>
2025-09-17 13:39:00 +0300

    ура работает статистико

--- ISIP423_GuydaANDMura/Program.cs
+++ ISIP423_GuydaANDMura/Program.cs
@@ -6,36 +6,22 @@ using System.Reflection.Metadata.Ecma335;
 class Program
 {
     static string[] tovari;
-    static string[] nazv = new string[40];
-    static double[] price = new double[40];
-
-    static void vvod()
-    {
-
-        Console.WriteLine("Введите количество операций: (от 2 до 40) ");
-        int kolvo = Convert.ToInt32(Console.ReadLine());
-        while (kolvo < 2 || kolvo > 40)
-        {
-            Console.WriteLine("Неверно! Введите другое количество!");
-            kolvo = Convert.ToInt32(Console.ReadLine());
-        }
-        tovari = new string[kolvo];
-        for (int i = 0; i < kolvo; i++)
-        {
-            Console.WriteLine("Введите название: ");
-            nazv[i] = Console.ReadLine();
-            Console.WriteLine("Введите цену: ");
-            price[i] = Convert.ToDouble(Console.ReadLine());
-            tovari[i] = nazv[i] + " " + price[i].ToString() + "руб.";
-        }
-    }
+    static string[] nazv;
+    static double[] price;
+    static int actualCount;
     static void Main()
     {
+        
         bool flag = true;
-        vvod();
-        Console.WriteLine("Введите число: 1 - вывести всю инф, 2 - статистика покупок, 0 - выход.");
-        int menu = Convert.ToInt32(Console.ReadLine());
+        Vvod();
+        while (flag)
         {
+            Console.WriteLine("Введите число: 1 - вывести всю инф, 2 - статистика покупок, 0 - выход.");
+            int menu = Convert.ToInt32(Console.ReadLine());
+            if (menu == 0)
+            {
+                flag = false;
+            }
             switch (menu)
             {
                 case 0:
@@ -47,10 +33,33 @@ class Program
                     }
                 case 2:
                     {
-                        statistika(price);
+                        statistika(price, actualCount);
                         break;
                     }
             }
+        }
+    }
+    static void Vvod()
+    {
+
+        Console.WriteLine("Введите количество операций: (от 2 до 40) ");
+        int kolvo = Convert.ToInt32(Console.ReadLine());
+        while (kolvo < 2 || kolvo > 40)
+        {
+            Console.WriteLine("Неверно! Введите другое количество!");
+            kolvo = Convert.ToInt32(Console.ReadLine());
+        }
+        actualCount = kolvo;
+        tovari = new string[kolvo];
+        nazv = new string[kolvo];
+        price = new double[kolvo]; ;
+        for (int i = 0; i < kolvo; i++)
+        {
+            Console.WriteLine("Введите название: ");
+            nazv[i] = Console.ReadLine();
+            Console.WriteLine("Введите цену: ");
+            price[i] = Convert.ToDouble(Console.ReadLine());
+            tovari[i] = nazv[i] + " " + price[i].ToString() + "руб.";
         }
     }
     static void vivod(string[] tovari)
@@ -60,21 +69,34 @@ class Program
             Console.WriteLine(tovar);
         }
     }
-    static void statistika(double [] price)
+    static void statistika(double[] price, int count)
     {
-        double min, max, avg, sum=0;
-        min = price[0];
-        max = price[price.Length - 1];
-        
+        if (count == 0)
+        {
+            Console.WriteLine("Нет данных для статистики!");
+            return;
+        }
+        double min = price[0];
+        double max = price[0];
+        double sum = 0;
+
         for (int i = 0; i < price.Length; i++)
         {
-            if (price[i] < min) { min = price[i]; }
-            if (price[i] > max) { max = price[i]; }
+            if (price[i] < min)
+            {
+                min = price[i];
+            }
+            if (price[i] > max)
+            {
+                max = price[i];
+            }
             sum += price[i];
         }
-        
-        int kolvo = price.Length;
-        avg = sum/ kolvo;
-        Console.WriteLine($"Максимальная сумма: {max}");
+
+        double avg = sum / price.Length;
+        Console.WriteLine($"Максимальная сумма покупки: {max}");
+        Console.WriteLine($"Минимальная сумма покупки: {min}");
+        Console.WriteLine($"Средняя сумма покупок: {avg}");
+        Console.WriteLine($"Общая сумма покупок: {sum}");
     }
 }
