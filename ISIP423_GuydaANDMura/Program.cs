239010f0e15947440156053cf8210ba0b73427a2
codemachine52 <82andrik887@mail.ru>
2025-09-29 10:54:05 +0300

    переделана структура кода, сделаны первые 4 задания

--- ISIP423_GuydaANDMura/Program.cs
+++ ISIP423_GuydaANDMura/Program.cs
@@ -3,15 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection.Metadata;
 
-var users = new List<User>
-{
-    new User("Иванов Иван Иванович",  "ivanovii",   new DateTime(1990, 5, 18), new List<string> { "Чтение", "Путешествия", "Программирование" }),
-    new User("Петров Петр Петрович",  "petrovpp",   new DateTime(1985, 10, 2), new List<string> { "Футбол", "Кулинария" }),
-    new User("Сидорова Анна Михайловна","sidorovana", new DateTime(2000, 3, 15), new List<string> { "Рисование", "Программирование" }),
-    new User("Смирнов Алексей Николаевич","smirnovan",new DateTime(1995, 7, 30), new List<string> { "Хоккей", "Фотография" }),
-    new User("Кузнецова Ольга Валерьевна","kuznetsovao",new DateTime(1992, 12, 12), new List<string> { "Йога", "Путешествия" })
-};
-
 public class User
 {
     public string FullName { get; set; }
@@ -26,14 +17,71 @@ public class User
         DateOfBirth = dateOfBirth;
         Hobbies = hobbies;
     }
+}
 
-    public void overThirty(List<User> users) 
+class Program
+{
+    public static void overThirty(List<User> users)
     {
-        DateTime now = DateTime.Now;
-        var olds = users.Where(users => ((now - users.DateOfBirth).TotalDays) / 365 > 30);
+        int now = DateTime.Now.Year;
+        var olds = users.Where(user => (now - user.DateOfBirth.Year) > 30).ToList();
         foreach (var old in olds)
         {
-            Console.WriteLine(old.FullName, old.DateOfBirth);
+            Console.WriteLine($"ФИО: {old.FullName}, возраст: {now - old.DateOfBirth.Year}");
         }
+    }
+
+    public static void Logins(List<User> users)
+    {
+        var logins = users.OrderBy(user => user.Login).ToList();
+        foreach (var user in logins)
+        {
+            Console.WriteLine(user.Login);
+        }
+    }
+
+    public static void Programists(List<User> users)
+    {
+        var programists = users.Where(user => (user.Hobbies.Contains("Программирование"))).ToList();
+        Console.WriteLine("Люди - программисты: ");
+        foreach (var user in programists)
+        {
+            Console.WriteLine($"{user.Login}, ФИО: {user.FullName}");
+        }
+    }
+
+    public static void BirthYear(List<User> users)
+    {
+        var yearGroups = users.GroupBy(user => user.DateOfBirth.Year).OrderBy(group => group.Key);
+
+        foreach (var user in yearGroups)
+        {
+            Console.WriteLine($"Год: {user.Key} -> {user.Count()} человек");
+        }
+    }
+
+
+
+    static void Main()
+    {
+        var users = new List<User>
+{
+    new User("Иванов Иван Иванович",  "ivanovii",   new DateTime(1990, 5, 18), new List<string> { "Чтение", "Путешествия", "Программирование" }),
+    new User("Петров Петр Петрович",  "petrovpp",   new DateTime(1985, 10, 2), new List<string> { "Футбол", "Кулинария" }),
+    new User("Сидорова Анна Михайловна","sidorovana", new DateTime(2000, 3, 15), new List<string> { "Рисование", "Программирование" }),
+    new User("Смирнов Алексей Николаевич","smirnovan",new DateTime(1995, 7, 30), new List<string> { "Хоккей", "Фотография" }),
+    new User("Кузнецова Ольга Валерьевна","kuznetsovao",new DateTime(1992, 12, 12), new List<string> { "Йога", "Путешествия" })
+};
+
+        Console.WriteLine("старше 30: ");
+        overThirty(users);
+        Console.WriteLine();
+        Console.WriteLine("логины по алфавиту:");
+        Logins(users);
+        Console.WriteLine();
+        Programists(users);
+        Console.WriteLine();
+        BirthYear(users);
+        Console.WriteLine();
     }
 }
