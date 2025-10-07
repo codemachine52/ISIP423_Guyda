c506e951efd4f0aba0c6f5190f40fcb5fe1d5e05
codemachine52 <82andrik887@mail.ru>
2025-10-07 12:08:21 +0300

    реализована (наполовину) функция добавления студента

--- ISIP423_GuydaANDMura/Program.cs
+++ ISIP423_GuydaANDMura/Program.cs
@@ -6,10 +6,10 @@ class Program
     {
         class People
         {
-            string name { get; set; } = "";
-            string surname { get; set; } = "";
-            int age { get; set; }
-            string phone { get; set; } = "";
+            protected string name { get; set; } = "";
+            protected string surname { get; set; } = "";
+            protected int age { get; set; }
+            protected string phone { get; set; } = "";
             protected string role { get; set; } = "";
 
             public string GetName() => name;
@@ -30,12 +30,96 @@ class Program
 
         class Student : People
         {
+            List <Student> students = new List<Student>();
             public Student()
             {
                 role = "Student";
             }
 
             public string GetRole() => role;
+
+            static void AddStudent()
+            {
+                Console.WriteLine("=== Добавление студента ===");
+                while (true)
+                {
+                    var student = new Student();
+                    while (true)
+                    {
+                        Console.WriteLine("Введите имя студента: ");
+                        string name = Console.ReadLine();
+                        if (!string.IsNullOrEmpty(name))
+                        {
+                            student.name = name;
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Имя не может быть пустым!");
+                        }
+                    }
+
+                    while (true)
+                    {
+                        Console.WriteLine("Введите фамилию студента: ");
+                        string surname = Console.ReadLine();
+                        if (!string.IsNullOrEmpty(surname))
+                        {
+                            student.surname = surname;
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Фамилия не может быть пустой!");
+                        }
+                    }
+
+                    while (true)
+                    {
+                        Console.WriteLine("Введите возраст студента: ");
+                        if (int.TryParse(Console.ReadLine(), out int age))
+                        {
+                            if (age > 0)
+                            {
+                                student.age = age;
+                                break;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Возраст не может быть отрицательным!");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Возраст должен быть числом!");
+                        }
+                    }
+
+                    while (true)
+                    {
+                        Console.WriteLine("Введите телефон студента: ");
+                        string phone = Console.ReadLine();
+                        if (!string.IsNullOrEmpty(phone))
+                        {
+                            if (phone.Length > 10)
+                            {
+                                student.phone = phone;
+                                break;
+                            }
+
+                            else
+                            {
+                                Console.WriteLine("Длина телефонного номера не соответствует стандарту!");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Имя не может быть пустым!");
+                        }
+                    }
+
+                }
+            }
         }
 
         class Courses
