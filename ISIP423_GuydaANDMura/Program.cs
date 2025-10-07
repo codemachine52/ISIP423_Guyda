5860dacd12e1f6c2ca994b8f8b0854b9dee9631d
codemachine52 <82andrik887@mail.ru>
2025-10-07 11:45:17 +0300

    добавлены классы для работы с системой

--- ISIP423_GuydaANDMura/Program.cs
+++ ISIP423_GuydaANDMura/Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    class University
+    {
+        class People
+        {
+            string name { get; set; } = "";
+            string surname { get; set; } = "";
+            int age { get; set; }
+            string phone { get; set; } = "";
+            protected string role { get; set; } = "";
+
+            public string GetName() => name;
+            public string GetSurname() => name;
+            public int GetAge() => age;
+            public string GetPhone() => phone;
+        }
+
+        class Teacher : People
+        {
+            public Teacher()
+            {
+                role = "Teacher";
+            }
+
+            public string GetRole() => role;
+        }
+
+        class Student : People
+        {
+            public Student()
+            {
+                role = "Student";
+            }
+
+            public string GetRole() => role;
+        }
+
+        class Courses
+        {
+            string title { get; set; } = "";
+            string Teacher { get; set; } = "";
+            string description { get; set; } = "";
+
+            public string GetTitle() => title;
+            public string GetTeacher() => Teacher;
+            public string GetDescription() => description;
+        }
+    }
+}
