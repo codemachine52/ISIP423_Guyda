15c4b2aa40c4cc3e872bff0f2025dc97b640c075
codemachine52 <82andrik887@mail.ru>
2025-10-09 14:42:28 +0300

    добавлены методы добавления учителей и вывода всех учителей

--- ISIP423_GuydaANDMura/Program.cs
+++ ISIP423_GuydaANDMura/Program.cs
@@ -33,13 +33,26 @@ class Program
 
         public class Teacher : People
         {
-
+            protected int id = 0;
+            protected static int NextID = 1;
+            float workExpernc {  get; set; }
+            double salary { get; set; }
+            string course { get; set; } = "";
             public Teacher()
             {
                 role = "Teacher";
+                id = NextID++;
             }
 
             public string GetRole() => role;
+            public int GetID() => id;
+            public float GetExp() => workExpernc;
+            public double GetSalary() => salary;
+            public string GetCourse() => course;
+
+            public float SetExp(float exp) => workExpernc = exp;
+            public double SetSalary(double salar) => salary = salar;
+            public string SetCourse(string cours) => course = cours;
         }
 
 
@@ -264,15 +277,181 @@ class Program
                 }
 
         static List<University.Student> students = new List<University.Student>();
+        static List<University.Teacher> teachers = new List<University.Teacher>();
 
         static public void PrintStudent()
             {
                 foreach (var student in students)
                 {
-                    Console.WriteLine($"id: {student.GetID()}, имя: {student.GetName()}, фамилия: {student.GetSurname()}, номер студ. билета: {student.GetStud()}, группа обучения: {student.GetGrouppa()}\n" +
+                    Console.WriteLine($"id: {student.GetID()}, роль: {student.GetRole()} имя: {student.GetName()}, фамилия: {student.GetSurname()}, дата рождения: {student.GetBirthday()},\n номер студ. билета: {student.GetStud()}, группа обучения: {student.GetGrouppa()}\n" +
                         $"курс обучения: {student.GetCourse()}, учится платно: {student.GetPaidEducation()}, город проживания: {student.GetCity()}, номер тел: {student.GetPhone()}");
                 }
             }
+
+    static public void GetTeacher()
+    {
+        
+            Console.WriteLine("===  Добавление учителя  ===");
+            bool continadd = true;
+        while (continadd)
+        {
+            var teacher = new University.Teacher();
+            while (true)
+            {
+                Console.WriteLine("Введите имя учителя: ");
+                string name = Console.ReadLine();
+                if (string.IsNullOrEmpty(name))
+                {
+                    teacher.SetName(name);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Имя должно быть введено верно!");
+                }
+            }
+            while (true)
+            {
+                Console.WriteLine("Введите фамилию учителя: ");
+                string surname = Console.ReadLine();
+                if (string.IsNullOrEmpty(surname))
+                {
+                    teacher.SetSurname(surname);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Фамилия должна быть введена верно!");
+                }
+            }
+
+            while (true)
+            {
+                Console.WriteLine("Введите дату рождения учителя: ");
+                if (DateOnly.TryParse(Console.ReadLine(), out DateOnly result))
+                {
+                    if ((DateTime.Now.Year - result.Year) > 0)
+                    {
+                        teacher.SetBirthday(result);
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Возраст не может быть отрицательным!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Возраст должен быть числом!");
+                }
+            }
+
+            while (true)
+            {
+                Console.WriteLine("Введите номер телефона учителя:");
+                string numb = Console.ReadLine();
+                if (string.IsNullOrEmpty(numb))
+                {
+                    teacher.SetPhone(numb);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Номер телефона не может быть пустым!");
+                }
+            }
+
+            while (true)
+            {
+                Console.WriteLine("Введите город учителя:");
+                string city = Console.ReadLine();
+                if (string.IsNullOrEmpty(city))
+                {
+                    teacher.SetCity(city);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Город не может быть пустым!");
+                }
+            }
+
+            while (true)
+            {
+                Console.WriteLine("Введите стаж учителя:");
+                if(float.TryParse(Console.ReadLine(),out float stag))
+                {
+                    teacher.SetExp(stag);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Стаж должен быть числом!");
+                }
+            }
+
+            while (true)
+            {
+                Console.WriteLine("Введите ЗП учителя:");
+                if (double.TryParse(Console.ReadLine(), out double zp))
+                {
+                    teacher.SetSalary(zp);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Зарплата должна быть числом!");
+                }
+            }
+
+            while (true)
+            {
+                Console.WriteLine("Введите курс учителя:");
+                string course = Console.ReadLine();
+                if (string.IsNullOrEmpty(course))
+                {
+                    teacher.SetCourse(course);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Курс не может быть пустым!");
+                }
+            }
+
+            if (teacher != null)
+            {
+                teachers.Add(teacher);
+                Console.WriteLine("Учитель успешно добавлен. Продолжить? (да/нет)");
+                string choice = Console.ReadLine();
+                if (!string.IsNullOrEmpty(choice))
+                {
+
+                    if (choice.ToLower() == "да")
+                    {
+                        continadd = true;
+                    }
+                    if (choice.ToLower() == "нет")
+                    {
+                        continadd = false;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Не все поля заполнены. Не удалось добавить студента.");
+            }
+        }
+    }
+
+     static public void PrintTeachers()
+    {
+        foreach (var teacher in teachers)
+        {
+            Console.WriteLine($"{teacher.GetID()}, роль: {teacher.GetRole()}, имя: {teacher.GetName()}, фамилия: {teacher.GetSurname()}, дата рождения: {teacher.GetBirthday()},\n" +
+                $"город проживания: {teacher.GetCity()}, номер телефона: {teacher.GetPhone()}, стаж: {teacher.GetExp()}, зарплата: {teacher.GetSalary()}, курс: {teacher.GetCourse()}");
+        }
+    }
 
             public class Courses
             {
@@ -294,6 +473,8 @@ class Program
             Console.WriteLine("\n=== Университет - Главное меню ===");
             Console.WriteLine("1. Добавить студента");
             Console.WriteLine("2. Показать всех студентов");
+            Console.WriteLine("3. Добавить учителя");
+            Console.WriteLine("4. Показать всех учителей");
             Console.WriteLine("0. Выход");
             Console.Write("Выберите действие: ");
 
@@ -305,6 +486,12 @@ class Program
                         break;
                     case 2:
                         PrintStudent();
+                        break;
+                    case 3:
+                        GetTeacher();
+                        break;
+                    case 4:
+                        PrintTeachers();
                         break;
                     case 0:
                         Console.WriteLine("Выход из программы...");
