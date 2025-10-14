5d5fffd98de419799ceff6113fc2420937e5a68f
codemachine52 <82andrik887@mail.ru>
2025-10-14 11:31:23 +0300

    добавлены классы: игрок, оружие, броня. У класса игрок реализованы методы получения урона, лечения, замены оружия и брони.

--- ISIP423_GuydaANDMura/Program.cs
+++ ISIP423_GuydaANDMura/Program.cs
@@ -1,2 +1,95 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    class Game
+    {
+        class Player
+        {
+            int HP { get; set; }
+            public Weapon CurrentWeapon { get; set; }
+            public Armor CurrentArmor { get; set; }
+
+            public Player(int StartHP)
+            {
+                HP = StartHP;
+                CurrentWeapon = new Weapon("Кулаки", 5);
+                CurrentArmor = new Armor("Легкая рубашка", 3);
+            }
+
+            public void TakeDamage(int damage)
+            {
+                HP -= damage;
+                if(HP < 0) HP = 0;
+            }
+
+            public void Heal(int amonth)
+            {
+                HP += amonth;
+            }
+
+            public void NewWeapon(Weapon newWeapon)
+            {
+                CurrentWeapon = newWeapon;
+            }
+            public void NewArmor(Armor newArmor)
+            {
+                CurrentArmor = newArmor;
+            }
+
+            public void ShowStats()
+            {
+                Console.WriteLine("======          СТАТИСТИКА ИГРОКА          ======");
+                Console.WriteLine($"Здоровье: {HP}");
+                Console.WriteLine($"Оружие: {CurrentWeapon}");
+                Console.WriteLine($"Броня: {CurrentArmor}");
+                Console.WriteLine("==============================\n");
+            }
+        }
+
+        class Weapon
+        {
+            string Name { get; set; } = "";
+            int Damage {  get; set; }
+
+            public Weapon(string name, int damage)
+            {
+                Name = name;
+                Damage = damage;
+            }
+        }
+
+        class Armor
+        {
+            string Name { get; set; } = "";
+            int Defense { get; set; }
+
+            public Armor(string name, int defense)
+            {
+                Name = name;
+                Defense = defense;
+            }
+        }
+
+        class Enemy
+        {
+            string Name { get; set; } = "";
+            int HP { get; set; }
+            int Attack { get; set; }
+            int Defense { get; set;}
+
+            public Enemy(string name, int attack, int hp, int defense)
+            {
+                Name = name;
+                HP = hp;
+                Attack = attack;
+                Defense = defense;
+            }
+
+            public void AttackPlayer(Player player)
+            {
+
+            }
+        }
+    }
+}
