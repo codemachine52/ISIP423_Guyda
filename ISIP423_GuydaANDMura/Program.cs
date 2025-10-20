bb65622a2e309286a81440e2e047d29cc4a9ecda
codemachine52 <82andrik887@mail.ru>
2025-10-20 10:10:21 +0300

    добавлены классы (боссы) Архемаг С++ и Пестов С--, наслединики класса Босс.

--- ISIP423_GuydaANDMura/Program.cs
+++ ISIP423_GuydaANDMura/Program.cs
@@ -219,5 +219,68 @@ class Program
             }
         }
 
+        class ArchmageCPP : Boss
+        {
+            double freezeChance = 0.35;
+            public bool FreezeApplied { get; private set; }
+
+            public ArchmageCPP() : base("Архимаг C++", (int)(12 * 1.6), (int)(20 * 1.8), (int)(1 * 1.1))
+            {
+                FreezeApplied = false;
+            }
+
+            public override void AttackPlayer(Player player)
+            {
+                Random random = new Random();
+                double damage = Attack;
+
+                player.TakeDamage((int)damage);
+                Console.WriteLine($"{Name} атакует и наносит {damage} урона!");
+
+                if (random.NextDouble() < freezeChance)
+                {
+                    FreezeApplied = true;
+                    Console.WriteLine($"{Name} накладывает заморозку! Вы пропустите следующий ход!");
+                }
+            }
+
+            public void ResetFreeze()
+            {
+                FreezeApplied = false;
+            }
+        }
+
+        class Pestov : Boss
+        {
+            double freezeChance = 0.4;
+            public bool FreezeApplied { get; private set; }
+
+            public Pestov() : base("Пестов С--", (int)(10 * 1.8), (int)(25 * 1.3), (int)(2 * 0.6))
+            {
+                FreezeApplied = false;
+            }
+
+            public override void AttackPlayer(Player player)
+            {
+                Random random = new Random();
+                double damage = Attack;
+
+                // Игнорирует защиту как скелет + шанс заморозки
+                player.TakeDamage(Attack);
+                Console.WriteLine($"{Name} игнорирует защиту и наносит {Attack} урона!");
+
+                if (random.NextDouble() < freezeChance)
+                {
+                    FreezeApplied = true;
+                    Console.WriteLine($"{Name} накладывает заморозку! Вы пропустите следующий ход!");
+                }
+            }
+
+            public void ResetFreeze()
+            {
+                FreezeApplied = false;
+            }
+        }
+
     }
 }
