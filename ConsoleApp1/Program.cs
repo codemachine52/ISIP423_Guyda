58a77c05ecf8b7f99e9310f5fee03e12b0dcef0e
codemachine52 <82andrik887@mail.ru>
2025-10-21 15:51:57 +0300

    добавлена База данных, в базовом методе main игроку добавлено начальное количество денег

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<cars> cars = Core.Context.cars.ToList();
+            player NewPlayer = new player
+            {
+                id = 1,
+                MyMoney = 2000
+            };
+
+        }
+    }
+}
