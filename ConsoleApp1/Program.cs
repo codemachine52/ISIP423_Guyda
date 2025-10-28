24f34e581c2b599c39c353a47ba5d0f04dbde175
codemachine52 <82andrik887@mail.ru>
2025-10-28 14:34:48 +0300

    написано главное меню игры и создан игрок, если по умолчанию пустой

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -17,7 +17,51 @@ namespace ConsoleApp1
 
         static void Main(string[] args)
         {
+            var player = Core.Context.player.FirstOrDefault();
+            if (player == null)
+            {
+                player = new player { MyMoney = 5000 };
+                Core.Context.player.Add(player);
+                Core.Context.SaveChanges();
+                Console.WriteLine("Создан новый игрок!");
+            }
+            bool gameRunning = true;
 
+            while (gameRunning)
+            {
+                Console.Clear();
+                ShowPlayerStatus(player);
+                Console.WriteLine("\n1 - Обслужить следующего клиента");
+                Console.WriteLine("2 - Купить запчасти");
+                Console.WriteLine("3 - Посмотреть склад");
+                Console.WriteLine("4 - Выход");
+
+                var choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        ProcessNextCar(player);
+                        break;
+                    case "2":
+                        ShowStoreMenu(player);
+                        break;
+                    case "3":
+                        ShowInventory(player);
+                        break;
+                    case "4":
+                        gameRunning = false;
+                        break;
+                }
+
+                if (choice != "4")
+                {
+                    Console.WriteLine("\nНажмите любую клавишу для продолжения...");
+                    Console.ReadKey();
+                }
+            }
+
+            Console.WriteLine("Игра завершена!");
         }
 
 
