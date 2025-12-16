d1884ef9e46244dcf53de784958971337ddfe8af
codemachine52 <82andrik887@mail.ru>
2025-12-16 10:29:14 +0300

    добавлены классы оружия и брони

---
+++ ISIP423_GuydaANDMura/Armor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISIP423_GuydaANDMura
+{
+    internal class Armor
+    {
+        public string Name { get; set; } = "";
+        public int Defense { get; set; }
+
+        public Armor(string name, int defense)
+        {
+            Name = name;
+            Defense = defense;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} (Защита: {Defense})";
+        }
+    }
+}
---
+++ ISIP423_GuydaANDMura/Weapon.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISIP423_GuydaANDMura
+{
+    internal class Weapon
+    {
+        public string Name { get; set; } = "";
+        public int Damage { get; set; }
+
+        public Weapon(string name, int damage)
+        {
+            Name = name;
+            Damage = damage;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} (Урон: {Damage})";
+        }
+    }
+}
